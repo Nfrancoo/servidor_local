685f3c7dc8f4bb0f57917c599fa66a87a6b5b7a6
Nfrancoo <francodelaroma2@gmail.com>
2025-11-14 03:02:49 -0300

    blabla

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/buscar", async (req, res) => {
+  try {
+    const query = req.query.q;
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&addressdetails=1&limit=1`;
+
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "MiAppIonic/1.0 (contacto@miemail.com)"
+      }
+    });
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Error en geocoding" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor Node corriendo en puerto 3000");
+});
